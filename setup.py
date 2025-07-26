f64b5c1b85e5ea5b87694113466d31d8592df65c
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2025-07-27 01:25:45 +0700

    Fix: Change Charminal version from 1.0.0 to 1.1.0

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup
 
-with open("README.md", 'r') as fh:
+with open("README.md", "r") as fh:
   long_description = fh.read()
 
 setup(
-  name='charminal',
-  version='1.0.0',
-  description='A library containing commonly used emojis for terminal messages',
+  name="charminal",
+  version="1.1.0",
+  description="A library containing commonly used emojis for terminal messages",
   long_description=long_description,
-  long_description_content_type='text/markdown',
-  py_modules=['charminal'],
-  author='pwnwas',
-  author_email='sulaimanfawwazak@gmail.com',
-  url='https://github.com/sulaimanfawwazak/Charminal',
+  long_description_content_type="text/markdown",
+  py_modules=["charminal"],
+  author="pwnwas",
+  author_email="sulaimanfawwazak@gmail.com",
+  url="https://github.com/sulaimanfawwazak/Charminal",
   classifiers=[
-    'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent'
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent"
   ],
 )
